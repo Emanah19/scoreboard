e53064416a7f9b269d7bd251307abddc7c66e9de
Emanah19 <zb69726@gmail.com>
2024-06-07 21:21:58 +0100

    deleted and added another files

--- index.js
+++ index.js
@@ -1,15 +1,58 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
+// Home  
 
-function increment() {
-    count += 1
-    countEl.textContent = count
+let homeCount = 0;
+let changeCount = document.getElementById("home--btn");
+
+function addOne() {
+    homeCount += 1;
+    changeCount.textContent = homeCount;
+}
+function addTwo() {
+    homeCount += 2;
+    changeCount.textContent = homeCount;
+}
+function addThree() {
+    homeCount += 3;
+    changeCount.textContent = homeCount;
 }
 
-function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
+
+
+// Guest 
+let guestCount = 0;
+let guestCountChange = document.getElementById("guest--btn");
+
+function guestAddOne() {
+    guestCount += 1;
+    guestCountChange.textContent = guestCount;
+}
+function guestAddTwo() {
+    guestCount += 2;
+    guestCountChange.textContent = guestCount;
+}
+function guestAddThree() {
+    guestCount += 3;
+    guestCountChange.textContent = guestCount;
+}
+
+
+// save scores 
+
+let saveHome , saveGuest;
+
+let domHome = document.getElementById("home--count")
+let domGuest = document.getElementById("guest--count")
+
+function saveClick() {
+    saveHome  = `${homeCount} -- `
+    domHome.textContent += saveHome;
+
+    saveGuest  = `${guestCount} -- `
+    domGuest.textContent += saveGuest;
+
+    changeCount.textContent = 0;
+    homeCount = 0;
+    guestCountChange.textContent = 0;
+    guestCount = 0;
+
 }
